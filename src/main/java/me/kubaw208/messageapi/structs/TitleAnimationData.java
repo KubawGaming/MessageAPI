45d98943ccb4d4df5ad676e203568c32d5737d3c
kubaw208git <kubaw208kubaw208@gmail.com>
2026-07-13 22:00:40 +0200

    add: TitleAnimationData constructor with Integer values (previous only Long could be used)

--- src/main/java/me/kubaw208/messageapi/structs/TitleAnimationData.java
+++ src/main/java/me/kubaw208/messageapi/structs/TitleAnimationData.java
@@ -1,19 +1,33 @@
 package me.kubaw208.messageapi.structs;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.Accessors;
+import org.jetbrains.annotations.Nullable;
 
 @Getter
 @Setter
 @Accessors(chain = true)
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class TitleAnimationData {
+
+    public TitleAnimationData(@Nullable String title, @Nullable String subtitle, @Nullable Long fadeIn, @Nullable Long stay, @Nullable Long fadeOut, @Nullable Long time) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.fadeIn = fadeIn;
+        this.stay = stay;
+        this.fadeOut = fadeOut;
+        this.time = time;
+    }
+
+    public TitleAnimationData(@Nullable String title, @Nullable String subtitle, @Nullable Integer fadeIn, @Nullable Integer stay, @Nullable Integer fadeOut, @Nullable Integer time) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.fadeIn = fadeIn == null ? null : fadeIn.longValue();
+        this.stay = stay == null ? null : stay.longValue();
+        this.fadeOut = fadeOut == null ? null : fadeOut.longValue();
+        this.time = time == null ? null : time.longValue();
+    }
 
     private String title;
     private String subtitle;
